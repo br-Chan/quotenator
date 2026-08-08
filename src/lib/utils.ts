6ef6da04a66086191d703db3824b67ada05d1027
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2026-08-08 15:07:13 +1200

    feat: implement stylised image render of a quote

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+// Function expects date to be of form DD/MM/YYYY
+export function formatDate(date: string): string {
+	const [day, month, year] = date.split("/");
+	const parsedDate = new Date(Number(year), Number(month) - 1, Number(day));
+	return Intl.DateTimeFormat("en-NZ", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	}).format(parsedDate);
+}
--- src/routes/q.$quoteRow.tsx
+++ src/routes/q.$quoteRow.tsx
@@ -1,6 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { Status } from "use-google-sheets";
 import { useQuotes } from "#/hooks/useQuotes";
+import { formatDate } from "#/lib/utils";
 
 export const Route = createFileRoute("/q/$quoteRow")({
 	component: RouteComponent,
@@ -22,12 +23,32 @@ function RouteComponent() {
 	const quote = quotes[parseInt(quoteRow, 10) - 1];
 
 	return (
-		<div className="p-8">
-			<h1 className="text-4xl font-bold">Quote Row: {quoteRow}</h1>
-			<p className="mt-4 text-lg">{quote.Quote}</p>
-			<p>{quote.Owner}</p>
-			<p>{quote.Context}</p>
-			<p>{quote.Date}</p>
+		<div className="p-8 container mx-auto items-center justify-center gap-2 flex flex-col">
+			<div className="w-lg flex flex-row justify-between py-2">
+				<Link
+					className="border rounded px-4 py-2 w-fit hover:bg-gray-100 hover:cursor-pointer"
+					to="/"
+				>
+					Back
+				</Link>
+				<button
+					type="button"
+					className="border rounded px-4 py-2 w-fit hover:bg-gray-100 hover:cursor-pointer"
+				>
+					Copy Image
+				</button>
+			</div>
+
+			<div className="relative flex flex-col items-center justify-center aspect-square w-lg p-4 bg-radial from-amber-100 to-amber-200">
+				<p className="mt-4 text-4xl text-center caudex-bold-italic">
+					{quote.Quote}
+				</p>
+				<div className="absolute bottom-4 items-center justify-center text-center">
+					<p>~</p>
+					<p className="text-lg italic">{quote.Owner},</p>
+					<p className="text-md">{formatDate(quote.Date)}</p>
+				</div>
+			</div>
 		</div>
 	);
 }
