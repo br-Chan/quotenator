049ae86c2dea5995feeeede2a8d8f17a85918029
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2026-08-18 22:46:20 +1200

    fix: remove xs breakpoint for vertical margin in quote image

--- src/routes/q.$quoteRow.tsx
+++ src/routes/q.$quoteRow.tsx
@@ -113,7 +113,7 @@ function RouteComponent() {
 					ref={quoteRef}
 					className="w-full relative flex flex-col items-center justify-center aspect-square p-4 bg-[radial-gradient(circle,var(--color-amber-100),var(--color-amber-200))] cursor-default"
 				>
-					<p className="xs:my-12 sm:my-16 md:my-20 text-2xl sm:text-3xl md:text-4xl text-center whitespace-pre-wrap caudex-bold-italic">
+					<p className="my-12 sm:my-16 md:my-20 text-2xl sm:text-3xl md:text-4xl text-center whitespace-pre-wrap caudex-bold-italic">
 						{quote.Quote}
 					</p>
 					<div className="absolute bottom-4 items-center justify-center text-center">
